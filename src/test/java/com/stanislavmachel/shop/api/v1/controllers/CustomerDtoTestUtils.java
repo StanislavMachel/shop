54979a94041cb9d360438ef9a70c4c6a932b7fbc
stanislavmachel <stanislav.machel@gmail.com>
2019-11-09 17:41:13 +0200

    DEV-7 Create Customer with POST

--- src/test/java/com/stanislavmachel/shop/api/v1/controllers/CustomerDtoTestUtils.java
+++ src/test/java/com/stanislavmachel/shop/api/v1/controllers/CustomerDtoTestUtils.java
@@ -6,10 +6,15 @@ import java.util.UUID;
 
 class CustomerDtoTestUtils {
 	static CustomerDto getCustomerDto(UUID id, String firstName, String lastName) {
+		CustomerDto customerDto = getCustomerDto(firstName, lastName);
+		customerDto.setUrl("/api/v1/customers/" + id);
+		return customerDto;
+	}
+
+	static CustomerDto getCustomerDto(String firstName, String lastName){
 		CustomerDto customerDto = new CustomerDto();
 		customerDto.setFirstName(firstName);
 		customerDto.setLastName(lastName);
-		customerDto.setUrl("/api/v1/customers/" + id);
 		return customerDto;
 	}
 }
